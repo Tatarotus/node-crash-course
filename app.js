ef01f350fc9979d32ef9b2f2ba9f3dae272e6e2b
Samuel Dantas <sam@samuelresolve.com>
2020-07-10 21:03:04 -0300

    EJS - data, partials & css

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 const express = require('express');
+const morgan = require('morgan');
 
+const blogs = [
+  {title: 'Blog One', snippet: 'Hello World'},
+  {title: 'Blog Two', snippet: 'Hello Country'},
+  {title: 'Blog Three', snippet: 'Hello State'},
+];
 // express app
 const app = express();
 
-// listen for requests
+// morgan for logs
+app.use(morgan('common'));
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//  default port for requests
 app.listen(3000);
 
+//routes
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index', { title: 'Main', blogs });
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About Me' });
 });
 
 // redirects
 app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+   res.redirect('/about');
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create Interesting Posts!' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', {title: '404'});
 });
