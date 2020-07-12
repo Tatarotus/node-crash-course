cccfb02533696c89082341a7c4cd1d7e1a310f55
Samuel Dantas <sam@samuelresolve.com>
2020-07-12 10:48:29 -0300

    middlewares - custom, build-in & from packages

--- app.js
+++ app.js
@@ -2,21 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 
 const blogs = [
-  {title: 'Blog One', snippet: 'Hello World'},
-  {title: 'Blog Two', snippet: 'Hello Country'},
-  {title: 'Blog Three', snippet: 'Hello State'},
+  { title: 'Blog One', snippet: 'Hello World' },
+  { title: 'Blog Two', snippet: 'Hello Country' },
+  { title: 'Blog Three', snippet: 'Hello State' },
 ];
 // express app
 const app = express();
+const PORT = 3000;
 
 // morgan for logs
-app.use(morgan('common'));
+//app.use(morgan('common'));
+app.use((req, res, next) => {
+  console.log('A new request is made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  console.log(`[ ${new Date()} ]`);
+  next();
+});
 
+app.use(express.static('public'));
 //register view engine
 app.set('view engine', 'ejs');
 
 //  default port for requests
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 //routes
 app.get('/', (req, res) => {
@@ -29,7 +41,7 @@ app.get('/about', (req, res) => {
 
 // redirects
 app.get('/about-us', (req, res) => {
-   res.redirect('/about');
+  res.redirect('/about');
 });
 
 app.get('/blogs/create', (req, res) => {
@@ -38,5 +50,5 @@ app.get('/blogs/create', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', {title: '404'});
+  res.status(404).render('404', { title: '404' });
 });
