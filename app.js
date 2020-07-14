b15c9d9d1a6c46a5ff551c74b275c72e4c33d953
Samuel Dantas <sam@samuelresolve.com>
2020-07-14 16:47:54 -0300

    CRUD - Create, Read, Update & Delete app

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 const Blog = require('./models/blogs');
-
 const app = express();
-
 const PORT = process.env.PORT;
 
+app.use(express.static('public'));
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhovv.gcp.mongodb.net/net_ninja>?retryWrites=true&w=majority`;
 // express app
 mongoose
@@ -20,24 +19,11 @@ mongoose
   )
   .catch(err => console.log(err));
 
-const blogs = [
-  { title: 'Blog One', snippet: 'Hello World' },
-  { title: 'Blog Two', snippet: 'Hello Country' },
-  { title: 'Blog Three', snippet: 'Hello State' },
-];
+//morgan for logs
+app.use(morgan('dev'));
+//serve public folder - mostly css now
+app.use(express.urlencoded({extented: true}));
 
-// morgan for logs
-//app.use(morgan('common'));
-app.use((req, res, next) => {
-  console.log('A new request is made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  console.log(`[ ${new Date()} ]`);
-  next();
-});
-
-app.use(express.static('public'));
 //register view engine
 app.set('view engine', 'ejs');
 
@@ -51,34 +37,47 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.get('/add-blog', async (req, res) => {
-  const blog = new Blog({
-    title: 'new blog #2',
-    snippet: 'about my new blog',
-    body: 'more about my new blog',
-  });
-  try {
-    response = await blog.save();
-    res.send(response);
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.get('/all-blogs/', (req, res) => {
-  res.redirect('/')
-});
-
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About Me' });
 });
 
+app.get('/create/', (req, res) => {
+  res.render('create', { title: 'Create Interesting Posts!' });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const {id } = req.params
+  Blog.findById(id)
+    .then((response) => {
+      res.render('single', {title: 'Blog Details', blog: response});
+    })
+})
+app.post('/blogs/', async (req, res) => {
+  const { title, snippet } = req.body;
+  const blog = new Blog({
+    title,
+    snippet,
+  });
+  try {
+    response = await blog.save();
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.delete('/blogs/:id', (req, res) => {
+  const { id } = req.params;
+  Blog.findByIdAndDelete(id)
+    .then((response) => res.json({redirect: '/'}))
+    .catch((err) => console.log(err))
+})
 // redirects
 app.get('/about-us', (req, res) => {
   res.redirect('/about');
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create Interesting Posts!' });
+app.get('/all-blogs/', (req, res) => {
+  res.redirect('/')
 });
 
 // 404 page
--- models/blogs.js
+++ models/blogs.js
@@ -6,7 +6,7 @@ const blogSchema = new Schema(
   {
     title: requiredString,
     snippet: requiredString,
-    body: requiredString,
+    //body: requiredString,
   },
   { timestamps: true }
 );
